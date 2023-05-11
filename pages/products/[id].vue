5f1130ae3739661ad4d2e271e940c9df7b6617d0
punkpoet <rahulramachandran462@gmail.com>
2023-05-11 07:40:17 +0530

    initial commit

---
+++ pages/products/[id].vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+<p>Prduct details for {{ id  }}</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat explicabo sequi dolorem? Eaque consectetur ut voluptas praesentium, sunt repellendus? Possimus.</p>
+    </div>
+</template>
+
+<script setup>
+
+const {id} = useRoute().params
+</script>
+
+<style scoped>
+ 
+</style>
